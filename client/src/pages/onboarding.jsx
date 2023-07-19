964e3d96030f922048ce50af1d86af42cee919db
João Heitor <78935085+jhmartins1@users.noreply.github.com>
2023-07-19 14:27:55 -0300

    chore: add initial onboard wpp

--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
 import React from "react";
 
 function onboarding() {
-  return <div>onboarding</div>;
+  return (
+    <div className="bg-panel-header-background h-screen w-screen text-white flex flex-col items-center justify-center">
+      <div className="flex items-center justify-center gap-2">
+        <Image src="/whatsapp.gif" alt="whatsapp" height={300} width={300} />
+      </div>
+    </div>
+  );
 }
 
 export default onboarding;
